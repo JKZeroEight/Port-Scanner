3619cfdc392b6372b7c75fcb4d04b90c7f50cf6b
JKZeroEight <jk.0cyber@gmail.com>
2026-06-17 12:53:51 +0545

    Update Port_Scanner.py

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -5,9 +5,12 @@ target = input("Enter target IP or domain: ")
 print("Scanning ports on", target)
 
 for port in range(1, 1025):
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+   
     """AF_INET → IPv4 (normal internet IPs)
     SOCK_STREAM → TCP connection (reliable connection-based protocol)"""
+    
     socket.setdefaulttimeout(0.1)
 
     result = s.connect_ex((target, port))
